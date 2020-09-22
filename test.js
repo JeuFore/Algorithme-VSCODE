a277a5c2e8df0d7329d72740e8a332db63af0f3c
JeuFore <mat.chast67@hotmail.fr>
2020-09-22 14:54:14 +0200

    First commit

---
+++ test.js
@@ -0,0 +1,23 @@
+function name(params) {
+    return;
+}
+
+while (condition) {
+    
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+if (condition ) {
+    return;
+} else {
+    
+}
+
+{
+    "name": "markup.underline.link.markdown, keyword.function",
+    "match": "\\b(fonction|fin|lexique)\\b"
+},
